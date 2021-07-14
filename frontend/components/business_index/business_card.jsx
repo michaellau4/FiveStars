912a9a400385010d3f2ce2df4f2845f1d9eb269f
Michael Lau <lau.michael2@gmail.com>
2021-07-13 18:29:28 -0700

    Added ratings to business cards

--- frontend/components/business_index/business_card.jsx
+++ frontend/components/business_index/business_card.jsx
@@ -1,41 +1,9 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel'
 
-// const BusinessCard = (props) => {
-//   let reviews = props.business.reviews();
-//   return (
-//     <div className="business-list-container">
-//       <div className="business-list">
-//         <div>
-//           {/* {props.business.photoUrls.map((photoUrl, i) => (
-//             <img key={i} src={photoUrl} key={i} alt="" />
-//           ))} */}
-//         </div>
-//         <span className="title-container">
-//           <h3>
-//             {(props.id + 1)}.&nbsp; 
-//           </h3>
-//           <h3 className="business-list-name">
-//             {props.business.business_name}
-//           </h3>
-//         </span>
-//         <div>
-//           {reviews}
-//         </div>
-//         <div className="business-list-tag-container">
-//           {props.business.tags.map((tag) => (
-//             <div className="business-list-tags" key={tag.id}>{tag.tag}</div>
-//           ))}
-//         </div>
-//       </div>    
-//     </div>
-//   )
-// };
-
 class BusinessCard extends React.Component {
   constructor(props) {
     super(props);
-    console.log(this.props);
   }
 
   render() {
@@ -46,6 +14,104 @@ class BusinessCard extends React.Component {
       avgRating += review.rating
     ));
     avgRating = avgRating / totalReviews;
+    let styleStars = {
+      width: "108px",
+      height: "20px",
+      display: "inlineBlock",
+      verticalAlign: "middle",
+      background: 'url("https://s3-media0.fl.yelpcdn.com/assets/public/stars_v2.yji-52d3d7a328db670d4402843cbddeed89.png")',
+      backgroundPosition: "0 -500px",
+    }
+    if (avgRating > 4.9) {
+
+    } else if (avgRating < 4.9 && avgRating >= 4.5) {
+        styleStars = {
+        width: "108px",
+        height: "20px",
+        display: "inlineBlock",
+        verticalAlign: "middle",
+        background: 'url("https://s3-media0.fl.yelpcdn.com/assets/public/stars_v2.yji-52d3d7a328db670d4402843cbddeed89.png")',
+        backgroundPosition: "0 -460px",
+      }
+      
+    } else if (avgRating < 4.5 && avgRating >= 4) {
+        styleStars = {
+        width: "108px",
+        height: "20px",
+        display: "inlineBlock",
+        verticalAlign: "middle",
+        background: 'url("https://s3-media0.fl.yelpcdn.com/assets/public/stars_v2.yji-52d3d7a328db670d4402843cbddeed89.png")',
+        backgroundPosition: "0 -480px",
+      }
+        
+    } else if (avgRating < 4 && avgRating >= 3.5) {
+        styleStars = {
+        width: "108px",
+        height: "20px",
+        display: "inlineBlock",
+        verticalAlign: "middle",
+        background: 'url("https://s3-media0.fl.yelpcdn.com/assets/public/stars_v2.yji-52d3d7a328db670d4402843cbddeed89.png")',
+        backgroundPosition: "0 -420px",
+      }
+      
+    } else if (avgRating < 3.5 && avgRating >= 3) {
+        styleStars = {
+        width: "108px",
+        height: "20px",
+        display: "inlineBlock",
+        verticalAlign: "middle",
+        background: 'url("https://s3-media0.fl.yelpcdn.com/assets/public/stars_v2.yji-52d3d7a328db670d4402843cbddeed89.png")',
+        backgroundPosition: "0 -440px"
+      }
+      
+    } else if (avgRating < 3 && avgRating >= 2.5) {
+        styleStars = {
+        width: "108px",
+        height: "20px",
+        display: "inlineBlock",
+        verticalAlign: "middle",
+        background: 'url("https://s3-media0.fl.yelpcdn.com/assets/public/stars_v2.yji-52d3d7a328db670d4402843cbddeed89.png")',
+        backgroundPosition: "0 -380px",
+      }
+    
+    } else if (avgRating < 2.5 && avgRating >= 2) {
+        styleStars = {
+        width: "108px",
+        height: "20px",
+        display: "inlineBlock",
+        verticalAlign: "middle",
+        background: 'url("https://s3-media0.fl.yelpcdn.com/assets/public/stars_v2.yji-52d3d7a328db670d4402843cbddeed89.png")',
+        backgroundPosition: "0 -400px",
+      }
+    } else if (avgRating < 2 && avgRating >= 1.5) {
+        styleStars = {
+        width: "108px",
+        height: "20px",
+        display: "inlineBlock",
+        verticalAlign: "middle",
+        background: 'url("https://s3-media0.fl.yelpcdn.com/assets/public/stars_v2.yji-52d3d7a328db670d4402843cbddeed89.png")',
+        backgroundPosition: "0 -340px",
+      }
+    } else if (avgRating < 1.5 && avgRating >= 1) {
+        styleStars = {
+        width: "108px",
+        height: "20px",
+        display: "inlineBlock",
+        verticalAlign: "middle",
+        background: 'url("https://s3-media0.fl.yelpcdn.com/assets/public/stars_v2.yji-52d3d7a328db670d4402843cbddeed89.png")',
+        backgroundPosition: "0 -360px",
+      }
+    } else {
+        styleStars = {
+        width: "108px",
+        height: "20px",
+        display: "inlineBlock",
+        verticalAlign: "middle",
+        background: 'url("https://s3-media0.fl.yelpcdn.com/assets/public/stars_v2.yji-52d3d7a328db670d4402843cbddeed89.png")',
+        backgroundPosition: "0 -320px",
+      }
+    }
+    let stars = <div style={styleStars}></div>
     if (!business) {
       return (
         <div>Loading...</div>
@@ -56,11 +122,11 @@ class BusinessCard extends React.Component {
         <div className="business-list">
           <div className="image-container">
             
-              <div className="image-carousel-container">
-                <img className="business-images" src={business.photoUrls[0]} alt="" />
-              </div>
-              
+            <div className="image-carousel-container">
+              <img className="business-images" src={business.photoUrls[0]} alt="" />
             </div>
+              
+          </div>
           <div className="business-info-container">
             <span className="title-container">
               <h3>
@@ -71,14 +137,18 @@ class BusinessCard extends React.Component {
               </h3>
             </span>
             <div>
-              <h3>
-                {business.reviews.length}
-              </h3>
+              <span className="business-card-rating">
+                <p className="business-card-stars">{stars}</p>
+                <p className="business-card-reviews">{business.reviews.length}</p>
+              </span>
             </div>
             <div className="business-list-tag-container">
               {business.tags.map((tag) => (
                 <div className="business-list-tags" key={tag.id}>{tag.tag}</div>
               ))}
+            </div>
+            <div>
+              {/* {(!business.reviews[0]) ? <p>{business.reviews[0].body}</p> : null} */}
             </div>
           </div>
         </div>  
